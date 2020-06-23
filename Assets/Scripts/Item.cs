86d2631f7cfbd40c275a4f8909212b7d0ef10eb6
DC <43692686+excaliburDC@users.noreply.github.com>
2020-06-23 13:14:34 +0530

    created ui for inventory

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Inventory/Add Item")]
+public class Item : ScriptableObject
+{
+    public string itemName = "";
+    public Sprite itemIcon;
+}
---
+++ Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    [SerializeField] private Image itemImage;
+
+    private Item _item;
+    public Item Item
+    {
+        get
+        {
+            return _item;
+        }
+
+        set
+        {
+            _item = value;
+
+            if(_item==null)
+            {
+                itemImage.enabled = false;
+
+            }
+            else
+            {
+                itemImage.sprite = _item.itemIcon;
+                itemImage.enabled = true;
+            }
+        }
+    }
+
+
+
+    private void OnValidate()
+    {
+        if (itemImage == null)
+            itemImage = GetComponent<Image>();
+    }
+}
