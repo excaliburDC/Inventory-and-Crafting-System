ca34f5af656dd058b191ba873cf1dc407c9a71cc
DC <43692686+excaliburDC@users.noreply.github.com>
2020-08-22 20:18:42 +0530

    added toggling of inventory

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject inventoryObj;
+    [SerializeField] private KeyCode[] toggleInventory;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < toggleInventory.Length; i++)
+        {
+            if(Input.GetKeyDown(toggleInventory[i]))
+            {
+                inventoryObj.SetActive(!inventoryObj.activeSelf);
+
+                break;
+            }
+        }
+    }
+}
